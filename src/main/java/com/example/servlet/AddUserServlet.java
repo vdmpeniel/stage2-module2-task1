f4731f18354eb7364379c4dd9b7e352432fb3e8a
victor diaz <vdmpeniel@yahoo.es>
2023-08-02 11:30:10 -0400

    Added logs and init params with the jsp paths

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -4,24 +4,41 @@ import com.example.User;
 import com.example.Warehouse;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Objects;
+import java.util.logging.Logger;
 
-@WebServlet(value="/add")
+@WebServlet(
+        value="/add",
+        initParams = {
+            @WebInitParam(name = "ADD_JSP_PATH", value = "/jsp/add.jsp")
+        }
+)
 public class AddUserServlet extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(AddUserServlet.class.getName());
+    private String addJspPath;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        addJspPath = config.getInitParameter("ADD_JSP_PATH");
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         try {
             // forward to add.jsp
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/add.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher(addJspPath);
             dispatcher.forward(request, response);
 
-        } catch(Exception e) {}
+        } catch(Exception e) {
+            logger.info("Error: " + e.getCause());
+        }
     }
 
     @Override
@@ -37,6 +54,9 @@ public class AddUserServlet extends HttpServlet {
 
             // forward to add.jsp
             doGet(request, response);
-        } catch(Exception e) {}
+
+        } catch(Exception e) {
+            logger.info("Error: " + e.getCause());
+        }
     }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -3,22 +3,41 @@ package com.example.servlet;
 import com.example.Warehouse;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.Logger;
 
-@WebServlet(value="/users")
+@WebServlet(
+        value="/users",
+        initParams = {
+                @WebInitParam(name = "USERS_JSP_PATH", value = "/jsp/users.jsp")
+        }
+)
 public class GetUsersServlet extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(GetUsersServlet.class.getName());
+    private String usersJspPath;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        usersJspPath = config.getInitParameter("USERS_JSP_PATH");
+    }
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response){
         try {
             request.setAttribute("users", Warehouse.getInstance().getUsers());
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/users.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher(usersJspPath);
             if (Objects.nonNull(dispatcher)) { dispatcher.forward(request, response); }
-        } catch(Exception e){}
+
+        } catch(Exception e){
+            logger.info("Error: " + e.getCause());
+        }
     }
 }
